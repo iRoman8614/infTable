5cb50a3fdd64a5f1aaa07320743c604957a0eda6
iRoman8614 <rom8614@gmail.com>
2025-09-01 17:15:19 +0300

    web component + react branch

--- src/component/web-table.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import r2wc from 'react-to-webcomponent';
-import { Table } from './table';
-
-const WebTable = r2wc(Table, React, ReactDOM);
-
-customElements.define('web-table', WebTable);
