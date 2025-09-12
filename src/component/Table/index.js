4e71020dd19237b500f2df0a1ab9606894118c48
iRoman8614 <rom8614@gmail.com>
2025-09-12 12:20:48 +0300

    refactoring + handleCellClick + headersprovider

--- src/component/Table/index.js
+++ src/component/Table/index.js
@@ -1,30 +1,3 @@
-// // Основные экспорты
-// export { Table as default } from './Table.jsx';
-// export { Table } from './Table.jsx';
-//
-// // Утилиты для данных
-// export { formatDate, parseDateString } from './utils/dateUtils.js';
-// export { setDataProvider, getDataProvider } from './utils/dataProcessing.js';
-//
-// // Утилиты для заголовков (НОВЫЕ)
-// export {
-//     setHeaderProvider,
-//     getHeaderProvider,
-//     getHeadersStructure,
-//     clearHeaderProvider
-// } from './utils/headerProcessing.js';
-//
-// // Компоненты (для кастомизации)
-// export { TableHeader } from './components/TableHeader.jsx';
-// export { FiltersPanel } from './components/FiltersPanel.jsx';
-//
-// // Хуки (для создания кастомных компонентов)
-// export { useTableLogic } from './hooks/useTableLogic.js';
-// export { useNodeVisibility } from './hooks/useNodeVisibility.js';
-//
-// // Данные по умолчанию
-// export { defaultHeaders } from './data/defaultData.js';
-
 // Основные экспорты
 export { Table as default } from './Table.jsx';
 export { Table } from './Table.jsx';
